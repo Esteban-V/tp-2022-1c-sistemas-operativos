9ff6fbc85506fff7b197157b405db2ec0f16ac3c
Dolores Lopez Joffre <dlopezjoffre@frba.utn.edu.ar>
2022-07-12 16:44:27 +0000

    Builder bash script para todos los modulos + carpetas de config para cada prueba

--- console/runner.sh
+++ console/runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Algunas validaciones iniciales
+# Valida que la carpeta de "codigo" exista y sea un directorio
 if [ -z "$1" ]
 then
-	echo Falta la carpeta con los programas a ejecutar
+	echo "Falta la carpeta con los programas a ejecutar"
 	exit 1
 fi
 
@@ -13,50 +13,38 @@ then
 	exit 1
 fi
 
+# Avisa del tamaño default de las consolas al no ser especificado
 if [ -z "$2" ]
 then
-	echo Falta el archivo de config a usar
-	exit 1
+	echo "Falta especificar el tamaño del proceso: USANDO 4096"
 fi
 
-if [ ! -f "$2" ]
-then
-	echo "El archivo de config $2 no existe :("
-	exit 1
-fi
-
-if [ -z "$3" ]
-then
-	echo Falta especificar el tamaño del proceso: USANDO TAMAÑO DE PROCESO 4096
-fi
-
-if [ -z "$4" ]
-then
-	echo Falta especificar la cantidad de iteraciones: ITERANDO 1 SOLA VEZ
-fi
-
-# Los parametros en bash se referencian segun el orden, $1, $2, etc
 FOLDER_WITH_PROGS=$1
-CONFIG_FILE=$2
-PROCESS_SIZE=${3:-4096}
-SCRIPTS_QTY=${4:-1}
-
-INTERVAL=0.5 # Para esperar, en segundos, entre cada ejecucion de la consola
-
+# Usa 4096 para cada consola si no fue especificado como 2do parametro
+PROCESS_SIZE=${2:-4096}
+# Realiza una sola iteracion de cada consola si no fue especificado como 3er parametro
+SCRIPTS_QTY=${3:-1}
 
 for i in `seq 1 $SCRIPTS_QTY`
 do
-	echo Iniciando iteracion $i...
+	if [ "$SCRIPTS_QTY" -gt "1" ]
+	then
+		echo Iniciando iteracion $i...
+	fi
+
 	for CODE in `ls $FOLDER_WITH_PROGS`
 	do
-		CODE_PATH=$FOLDER_WITH_PROGS/$CODE
-		cp $CONFIG_FILE ./cfg/console.config
-		LOG_PATH=./cfg/console_$CODE-$i.log
-		echo Iniciando consola $CODE-$i en background y enviando log a $LOG_PATH...
+		#Crea la carpeta de logs si no existe, y redirecciona los logs de cada consola alli
+		mkdir -p ./run-logs
+		LOG_PATH=./run-logs/console-$CODE-$i.log
 
-		# Cada ejecucion redirecciona la salida estándar (la terminal) a un archivo propio 
+		echo "Iniciando consola $CODE-$i en background y enviando log a $LOG_PATH..."
+
+		# Obtiene el "codigo" a correr, y ejecuta la consola
+		CODE_PATH=$FOLDER_WITH_PROGS/$CODE
 		./console $CODE_PATH $PROCESS_SIZE  >$LOG_PATH &
 
-		sleep $INTERVAL
+		# Espera el intervalo especificado (en segundos) para ejecutar la proxima consola
+		sleep 0.5
 	done
 done 
